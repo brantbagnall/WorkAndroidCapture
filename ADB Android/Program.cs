0495334f140796ba36961771d8ce948327760349
brantbagnall <brant.bagnall@gmail.com>
2018-10-11 17:10:16 -0700

    Added adb PATH code and ability to add custom adb path information

--- ADB Android/Program.cs
+++ ADB Android/Program.cs
@@ -16,13 +16,39 @@ namespace ADB_Android
     {
         static void Main()
         {
+            Console.WriteLine("Loading...");
             int nameNumberVideo = 1;
             int nameNumberImg = 1;
             var adb = SharpAdbClient.AdbClient.Instance;
             int i = 0;
             bool auto = true;
+            var pathValues = Environment.GetEnvironmentVariable("PATH");
+            string adbPath = "none";
+            foreach (var path in pathValues.Split(';'))
+            {
+                var fullPath = Path.Combine(path, "adb.exe");
+                if (File.Exists(fullPath))
+                {
+                    adbPath = fullPath.ToString();
+                }
+            }
             AdbServer server = new AdbServer();
-            var result = server.StartServer(@"./Android-tools/adb.exe", restartServerIfNewer: true);
+            if (adbPath == "none")
+            {
+                if (File.Exists(Path.GetFullPath("adb.exe")))
+                {
+                    adbPath = Path.GetFullPath("adb.exe").ToString();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter the full path to an adb executable then press Enter:");
+                    adbPath = Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("Loading...");
+                }             
+            }
+            var result = server.StartServer(adbPath, restartServerIfNewer: true);
             while (i == 0) 
             {
                 Console.Clear();
@@ -45,108 +71,121 @@ namespace ADB_Android
 
                 if (key.KeyChar.ToString() == "c" && auto == false || key.KeyChar.ToString() == "C" && auto == false )
                 {
-                    var device = adb.GetDevices().First();
-                    var receiver = new ConsoleOutputReceiver();
-                    adb.ExecuteRemoteCommand("screencap /sdcard/capturescript.png", device, receiver);
-                    Console.WriteLine(receiver.ToString());
-                    Console.WriteLine("Please enter the name of the PNG image file:");
-                    string renameImg = Console.ReadLine().ToString();
-
-                    using (SyncService service = new SyncService(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)), device))
-                    using (Stream stream = File.OpenWrite(@"./" + renameImg + ".png"))
-                    {
-                        service.Pull("/sdcard/capturescript.png", stream, null, CancellationToken.None);
-                    }
-                    adb.ExecuteRemoteCommand("rm /sdcard/capturescript.png", device, receiver);
+                    nameScreen();
                 }
                 if (key.KeyChar.ToString() == "v" && auto == false || key.KeyChar.ToString() == "V" && auto == false)
                 {
-                    var device = adb.GetDevices().First();
-                    var receiver = new ConsoleOutputReceiver();
-                    CancellationTokenSource cancelSource = new CancellationTokenSource();
-                    CancellationToken cancel = cancelSource.Token;
-                    adb.ExecuteRemoteCommandAsync("screenrecord /sdcard/videocapturescript.mp4", device, receiver, cancel, 0);
-                    Console.WriteLine(receiver.ToString());
-                    Console.WriteLine("Press T to end recording and transfer the file to the pc.");
-
-                    ConsoleKeyInfo transfer = Console.ReadKey(true);
-                    bool transfered = false;
-                    while (transfered != true)
-                    {
-                        
-                        if (transfer.KeyChar.ToString() == "t" || transfer.KeyChar.ToString() == "T")
-                        {
-                            cancelSource.Cancel();
-                            Console.WriteLine("Please enter the name of the mp4 video file:");
-                            string renameVideo = Console.ReadLine().ToString();
-                            using (SyncService service = new SyncService(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)), device))
-                            using (Stream stream = File.OpenWrite(@"./" + renameVideo + ".mp4"))
-                            {
-                                service.Pull("/sdcard/videocapturescript.mp4", stream, null, CancellationToken.None);
-                            }
-                            adb.ExecuteRemoteCommand("rm /sdcard/videocapturescript.mp4", device, receiver);
-                            transfered = true;
-                        }
-                    }
-
+                    nameVideo();
                 }
 
                 if (key.KeyChar.ToString() == "c" && auto == true || key.KeyChar.ToString() == "C" && auto == true)
                 {
-                    var device = adb.GetDevices().First();
-                    var receiver = new ConsoleOutputReceiver();
-
-                    adb.ExecuteRemoteCommand("screencap /sdcard/capturescript.png", device, receiver);
-                    Console.WriteLine(receiver.ToString());
-                    System.Threading.Thread.Sleep(1000);
-                    using (SyncService service = new SyncService(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)), device))
-                    using (Stream stream = File.OpenWrite(@"./Image " + nameNumberImg + ".png"))
-                    {
-                        service.Pull("/sdcard/capturescript.png", stream, null, CancellationToken.None);
-                    }
-
-                    adb.ExecuteRemoteCommand("rm /sdcard/capturescript.png", device, receiver);
-                    nameNumberImg++;
+                    autoScreen();
                 }
                 if (key.KeyChar.ToString() == "v" && auto == true || key.KeyChar.ToString() == "V" && auto == true)
                 {
-                    var device = adb.GetDevices().First();
-                    var receiver = new ConsoleOutputReceiver();
-                    CancellationTokenSource cancelSource = new CancellationTokenSource();
-                    CancellationToken cancel = cancelSource.Token;
-                    adb.ExecuteRemoteCommandAsync("screenrecord /sdcard/videocapturescript.mp4", device, receiver, cancel, 0);
-                    Console.WriteLine(receiver.ToString());
-                    Console.WriteLine("Press T to end recording and transfer the file to the pc.");
-
-                    ConsoleKeyInfo transfer = Console.ReadKey(true);
-                    bool transfered = false;
-                    while (transfered != true)
-                    {
-
-                        if (transfer.KeyChar.ToString() == "t" || transfer.KeyChar.ToString() == "T")
-                        {
-                            cancelSource.Cancel();
-                            System.Threading.Thread.Sleep(1000);
-                            using (SyncService service = new SyncService(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)), device))
-                            using (Stream stream = File.OpenWrite(@"./Video " + nameNumberVideo + ".mp4"))
-                            {
-                                service.Pull("/sdcard/videocapturescript.mp4", stream, null, CancellationToken.None);
-                            }
-                            adb.ExecuteRemoteCommand("rm /sdcard/videocapturescript.mp4", device, receiver);
-                            transfered = true;
-                            nameNumberVideo++;
-                        }
-                    }
-
+                    autoVideo();
                 }
 
                 if (key.KeyChar.ToString() == "e" || key.KeyChar.ToString() == "E")
                 {
-                    Environment.Exit(0);
-                    
+                    adb.KillAdb();
+                    Environment.Exit(0); 
                 }
             }
-        }
-            
+            void nameScreen()
+            {
+                var device = adb.GetDevices().First();
+                var receiver = new ConsoleOutputReceiver();
+                adb.ExecuteRemoteCommand("screencap /sdcard/capturescript.png", device, receiver);
+                Console.WriteLine(receiver.ToString());
+                Console.WriteLine("Please enter the name of the PNG image file:");
+                string renameImg = Console.ReadLine().ToString();
+
+                using (SyncService service = new SyncService(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)), device))
+                using (Stream stream = File.OpenWrite(@"./" + renameImg + ".png"))
+                {
+                    service.Pull("/sdcard/capturescript.png", stream, null, CancellationToken.None);
+                }
+                adb.ExecuteRemoteCommand("rm /sdcard/capturescript.png", device, receiver);
+            }
+            void autoScreen()
+            {
+                var device = adb.GetDevices().First();
+                var receiver = new ConsoleOutputReceiver();
+
+                adb.ExecuteRemoteCommand("screencap /sdcard/capturescript.png", device, receiver);
+                Console.WriteLine(receiver.ToString());
+                System.Threading.Thread.Sleep(1000);
+                using (SyncService service = new SyncService(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)), device))
+                using (Stream stream = File.OpenWrite(@"./Image " + nameNumberImg + ".png"))
+                {
+                    service.Pull("/sdcard/capturescript.png", stream, null, CancellationToken.None);
+                }
+
+                adb.ExecuteRemoteCommand("rm /sdcard/capturescript.png", device, receiver);
+                nameNumberImg++;
+            }
+            void nameVideo()
+            {
+                var device = adb.GetDevices().First();
+                var receiver = new ConsoleOutputReceiver();
+                CancellationTokenSource cancelSource = new CancellationTokenSource();
+                CancellationToken cancel = cancelSource.Token;
+                adb.ExecuteRemoteCommandAsync("screenrecord /sdcard/videocapturescript.mp4", device, receiver, cancel, 0);
+                Console.WriteLine(receiver.ToString());
+                Console.WriteLine("Press T to end recording and transfer the file to the pc.");
+
+                ConsoleKeyInfo transfer = Console.ReadKey(true);
+                bool transfered = false;
+                while (transfered != true)
+                {
+
+                    if (transfer.KeyChar.ToString() == "t" || transfer.KeyChar.ToString() == "T")
+                    {
+                        cancelSource.Cancel();
+                        Console.WriteLine("Please enter the name of the mp4 video file:");
+                        string renameVideo = Console.ReadLine().ToString();
+                        using (SyncService service = new SyncService(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)), device))
+                        using (Stream stream = File.OpenWrite(@"./" + renameVideo + ".mp4"))
+                        {
+                            service.Pull("/sdcard/videocapturescript.mp4", stream, null, CancellationToken.None);
+                        }
+                        adb.ExecuteRemoteCommand("rm /sdcard/videocapturescript.mp4", device, receiver);
+                        transfered = true;
+                    }
+                }
+            }
+            void autoVideo()
+            {
+                var device = adb.GetDevices().First();
+                var receiver = new ConsoleOutputReceiver();
+                CancellationTokenSource cancelSource = new CancellationTokenSource();
+                CancellationToken cancel = cancelSource.Token;
+                adb.ExecuteRemoteCommandAsync("screenrecord /sdcard/videocapturescript.mp4", device, receiver, cancel, 0);
+                Console.WriteLine(receiver.ToString());
+                Console.WriteLine("Press T to end recording and transfer the file to the pc.");
+
+                ConsoleKeyInfo transfer = Console.ReadKey(true);
+                bool transfered = false;
+                while (transfered != true)
+                {
+
+                    if (transfer.KeyChar.ToString() == "t" || transfer.KeyChar.ToString() == "T")
+                    {
+                        cancelSource.Cancel();
+                        System.Threading.Thread.Sleep(1000);
+                        using (SyncService service = new SyncService(new AdbSocket(new IPEndPoint(IPAddress.Loopback, AdbClient.AdbServerPort)), device))
+                        using (Stream stream = File.OpenWrite(@"./Video " + nameNumberVideo + ".mp4"))
+                        {
+                            service.Pull("/sdcard/videocapturescript.mp4", stream, null, CancellationToken.None);
+                        }
+                        adb.ExecuteRemoteCommand("rm /sdcard/videocapturescript.mp4", device, receiver);
+                        transfered = true;
+                        nameNumberVideo++;
+                    }
+                }
+            }
+        }            
     }
 }
